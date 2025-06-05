0d9ddcd2e11d5626216c90b192616f932bc3fdce
Ekastyabudi <ekasetyabudixtkj3@gmail.com>
2025-06-05 19:20:23 +0700

    featured:server

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-const { admin } = require('./config/firebase');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+const { admin } = require("./config/firebase");
 
 const init = async () => {
   try {
     const server = Hapi.server({
       port: process.env.PORT || 3005,
-      host: '0.0.0.0' || 'localhost',
+      host: "0.0.0.0.0",
       routes: {
         cors: {
-          origin: ['*'],
-          headers: ['Accept', 'Content-Type', 'Authorization'],
-          additionalHeaders: ['X-Requested-With'],
-          credentials: true
-        }
-      }
+          origin: ["*"],
+          headers: ["Accept", "Content-Type", "Authorization"],
+          additionalHeaders: ["X-Requested-With"],
+          credentials: true,
+        },
+      },
     });
 
     server.route(routes);
 
     await server.start();
     console.log(`Server running on ${server.info.uri}`);
-
   } catch (error) {
-    console.error('Error starting server:', error);
+    console.error("Error starting server:", error);
     process.exit(1);
   }
 };
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.error(err);
   process.exit(1);
 });
